5956a7a2adb961ff064d6700b56c23620f390c78
Jeff Sparkes <jsparkes@gmail.com>
2018-05-14 02:49:43 -0400

    Initial project created by ProjectScaffold.

---
+++ tests/red7.Tests/Tests.fs
@@ -0,0 +1,10 @@
+module red7.Tests
+
+open red7
+open NUnit.Framework
+
+[<Test>]
+let ``hello returns 42`` () =
+  let result = Library.hello 42
+  printfn "%i" result
+  Assert.AreEqual(42,result)
